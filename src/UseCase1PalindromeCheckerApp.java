a503ea3475aa202e736ca060c478550f6b402cbb
Anant Agarwal <69358268+anant-agarwal12@users.noreply.github.com>
2026-02-23 10:37:06 +0530

    Removed UC1 and UC2
    Removed welcome message and hardcoded palindrome checker methods from the main application.

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -3,36 +3,7 @@ import java.util.*;
 public class UseCase1PalindromeCheckerApp{
 
     public static void main(String args[]){
-        welcomeMessage();
-        hardcodedPalindrome();
         stringReverse();
-    }
-
-    public static void welcomeMessage(){
-        System.out.println("Welcome to the Palindrome Checker System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully\n");
-    }
-
-    public static void hardcodedPalindrome(){
-        String test = "madam";
-        System.out.println("Hardcoded Palindrome Checker:");
-        System.out.println("Input text: madam");
-
-        int j=test.length()-1;
-        boolean notPalindrome = false;
-        for(int i=0 ; i<test.length()/2 ; i++){
-            if(test.charAt(i)!=test.charAt(j)) {
-                notPalindrome = true;
-                break;
-            }
-            j--;
-        }
-
-        if(notPalindrome)
-            System.out.println("Is it a Palindrome? : false\n");
-        else
-            System.out.println("Is it a Palindrome? : true\n");
     }
 
     public static void stringReverse(){
