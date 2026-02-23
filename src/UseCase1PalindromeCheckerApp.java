16c02004cf9071c2bee5b1076d8759a3dda4bd5a
--global <--global>
2026-02-23 09:07:44 +0530

    UC1:Add welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+// Use Case 1 - Application Entry & Welcome Message
+
+public class UseCase1PalindromeCheckerApp{
+    public static void main(String args[]){
+        System.out.println("Welcome to the Palindrome Checker System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+
+}
