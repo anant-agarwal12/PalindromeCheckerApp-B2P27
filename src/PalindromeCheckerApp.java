08abc9375fbbe0d981c29ad1bc97bb667c50dd7e
--global <--global>
2026-02-23 11:18:07 +0530

    Changes added

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,7 @@
 public class PalindromeCheckerApp {
 
-    public static void main(String args[]){
-        welcomeMessage();
+    public static void main(String[] args[{
         hardcodedPalindrome();
-    }
-
-    public static void welcomeMessage(){
-        System.out.println("Welcome to the Palindrome Checker System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully\n");
     }
 
     public static void hardcodedPalindrome(){
